87a18f7297df1310785b280bebc8029f0b27d8c4
houzairkoussa <houzairmk@icloud.com>
2023-07-29 22:39:28 +0400

    [dev] ADD numerical operator tests

--- arithmetic-with-functions/index.test.js
+++ arithmetic-with-functions/index.test.js
@@ -16,4 +16,31 @@ const callBack = () => {
   callBackCalls = callBackCalls + 1;
 }
 
+describe('Nummeral Encodings', () => {
+
+  beforeEach(() => {
+    callBackCalls = 0;
+  });
+
+  test('zero <=> 0', () => {
+    zero(callBack)();
+    expect(callBackCalls).toBe(0);
+  });
+
+  test('one <=> 1', () => {
+    one(callBack)();
+    expect(callBackCalls).toBe(1);
+  });
+
+  test('two <=> 2', () => {
+    two(callBack)();
+    expect(callBackCalls).toBe(2);
+  });
+
+  test('three <=> 3', () => {
+    three(callBack)();
+    expect(callBackCalls).toBe(3);
+  });
+
+});
 
